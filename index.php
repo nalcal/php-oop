597f9f3b776b33cd3aef00a5389860c04ec05344
Nalitha Liyanage <nalitha770@gmail.com>
2015-01-08 11:02:46 -0800

    lesson 1

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+    class Tiger {
+
+        //class body
+
+        //class body
+
+        //class body
+
+    }
+    
+    class Lion {
+
+        //class body
+
+        //class body
+
+        //class body
+
+    }
+    
+    class Puma {
+
+        //class body
+
+        //class body
+
+        //class body
+
+    }
+    
+   $tiger1 = new Tiger();
+   $tiger2 = new Tiger();
+   
+   $lion1 = new Lion();
+   $lion2 = new Lion();
+
+   $puma1 = new Puma();
+   $puma2 = new Puma();
+   
+   class Tiger {
+
+public $location = â€œIndiaâ€;
+
+public $species = â€œcatâ€;
+
+public $gender = â€œmaleâ€;
+
+public $population = 140;
+
+}
+
+class Lion {
+
+public $location = â€œAfricaâ€;
+
+public $species = â€œcatâ€;
+
+public $gender = â€œmaleâ€;
+
+public $population = 1400;
+
+}
+
+class Puma {
+
+public $location = â€œSouth Americaâ€;
+
+public $species = â€œcatâ€;
+
+public $gender = â€œmaleâ€;
+
+public $population = 1500;
+
+}
+
+$tiger1 = new Tiger();
+
+print $tiger1Â­>name;
+
+// default name
+
+$lion1 = new Lion();
+
+print $lion1Â­>name;
+
+// default name
+
+$puma1 = new Puma();
+
+print $puma1Â­>name;
+
+// default name
+
+public function myMethod( $argument, $another) {
+
+// stuff
+
+}
